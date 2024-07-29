eba62118135e3e513ec5d6aaadea4df07b02cccf
ryanzhao2 <ryan.zhao2@gmail.com>
2024-07-29 17:34:50 -0400

    change api_url

--- forecast.py
+++ forecast.py
@@ -17,7 +17,7 @@ class StockRequest(BaseModel):
     epochs: int
 
 # Define the prediction endpoint
-@app.post('/LSTM_Predict')
+@app.post('/TrendTracker')
 async def predict(stock_request: StockRequest):
     stock_name = stock_request.stock_name
     epochs = stock_request.epochs
