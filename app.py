75b748c42a83f4f444827d1492d26d6aced6f898
ryanzhao2 <ryan.zhao2@gmail.com>
2024-07-30 17:29:42 -0400

    change local url to render url

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from helper import *
 
 # Constants
 #API_URL is the API endpoint to get LSTM predictions
-API_URL = "http://127.0.0.1:8000/LSTM_Predict"
+API_URL = "https://trendtracker-uvqu.onrender.com/LSTM_Predict"
 MIN_DATE = datetime.date(2020, 1, 1)
 MAX_DATE = datetime.date(2022, 12, 31)
 DATA_DIR = 'data'
